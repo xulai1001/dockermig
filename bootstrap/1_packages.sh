811a1e7ba3a52bbd1e9f814e1c0bb09961fabe5a
viktorxu <xulai1001@gmail.com>
2019-03-04 20:55:45 +0800

    Mar 4

--- bootstrap/1_packages.sh
+++ bootstrap/1_packages.sh
@@ -2,7 +2,7 @@
 sudo apt install -y vim tree pv curl ruby python gcc g++ build-essential golang lrzsz lxc criu \
                 go-md2man asciidoc xmlto
 sudo apt install -y libseccomp-dev libprotobuf-dev libprotobuf-c0-dev protobuf-c-compiler protobuf-compiler python-protobuf pkg-config \
-                python-ipaddress libbsd0 iproute2 libcap-dev libnet1-dev libaio-dev python-yaml libnl-3-dev python-future texlive-latex-base
+                python-ipaddress libbsd0 iproute2 libcap-dev libnet1-dev libaio-dev python-yaml libnl-3-dev python-future texlive-latex-base ipvsadm
 
 echo "- install docker..."
 curl -sSL https://get.daocloud.io/docker |sh
